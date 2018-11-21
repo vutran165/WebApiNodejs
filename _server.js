836dbcb6b0d90226d0209020ef41f994994953a6
Trần Tuấn Vũ <vutran165@gmail.com>
2018-11-22 00:23:04 +0700

    1-22112018

--- _server.js
+++ _server.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
     // req.xhr;
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE");
     res.header("Access-Control-Allow-Credentials", true)
     next();
 })
